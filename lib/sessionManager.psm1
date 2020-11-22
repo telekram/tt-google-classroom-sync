63fe0d574a025fc0be1fa3d5541d91a8b0bb168b
Mark Lee <mlee.aus@gmail.com>
2020-11-22 12:55:13 +1100

    added session manager module to make it easer to run script from remote machines

--- lib/sessionManager.psm1
+++ lib/sessionManager.psm1
@@ -1,6 +1,6 @@
 $script:config = Get-Content -Raw -Path .\config.json | ConvertFrom-Json
 
-function Get-ScriptSessionType () {
+function Get-ScriptPSSession () {
   $isRemoteSession = [System.Convert]::ToBoolean($script:config.remoteSession)
 
   $sessionUser = $script:config.username
@@ -16,4 +16,17 @@ function Get-ScriptSessionType () {
   }
 
   return $session
+}
+
+function Clear-ScriptPSSession {
+  $isRemoteSession = [System.Convert]::ToBoolean($script:config.remoteSession)
+
+  $sessionUser = $script:config.username
+  $sessionPWord = ConvertTo-SecureString -String $script:config.password -AsPlainText -Force
+
+  $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $sessionUser, $sessionPWord
+  if ($isRemoteSession) {
+    $r = Get-PSSession -ComputerName gads1.curric.cheltenham-sc.wan -UseSSL -Credential $cred
+    $r | Remove-PSSession
+  }
 }
